afc78d9178117270575bdd79bf5fc711c4df8f24
Sebastián Silva <77755597+hsilv@users.noreply.github.com>
2021-11-16 23:57:31 +0000

    Se comenzó a implementar la secuencia de un radio normal

---
+++ Controlador.java
@@ -0,0 +1,44 @@
+
+public class Controlador {
+
+    public static void main(String[] args) {
+        Vista v = new Vista();
+        int index_radio = 0;
+        int index_accion1 = 0;
+        int index_accion2 = 0;
+        index_radio = v.menu_tipo();
+        Radio radio;
+
+        if (index_radio == 1) {
+            radio = new Radio();
+            System.out.println(radio);
+            while (index_accion1 != 6) {
+                index_accion1 = v.menu_p(radio);
+                if (index_accion1 == 1) {
+                    radio.turn();
+                    System.out.println(radio);
+                } else if (index_accion1 == 2 && radio.isTurn()) {
+                    radio.vol(v.menu_vol());
+                    System.out.println(radio);
+                } else if (index_accion1 == 3 && radio.isTurn()) {
+                    while (index_accion2 != 5) {
+                        index_accion2 = v.menu_r();
+                        if (index_accion2 == 1) {
+                            radio.Switch();
+                            System.out.println(radio);
+                        } else if (index_accion2 == 2) {
+                            radio.cambiarEmisora(v.menu_emi());
+                            System.out.println(radio);
+                        } else if (index_accion2 == 3) {
+                            radio.guardarEmisora(v.nombre_emisora());
+                            System.out.println(radio);
+                        } else if (index_accion2 == 4) {
+                            radio.cargarEmisora(v);
+                            System.out.println(radio);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
