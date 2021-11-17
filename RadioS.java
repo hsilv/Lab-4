1d3097a0aef765f49042401283681ae128fddf11
Sebastián Silva <77755597+hsilv@users.noreply.github.com>
2021-11-17 04:35:52 +0000

    Se agregó el radio de clase S

---
+++ RadioS.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+
+public class RadioS extends Radio {
+
+    private boolean bocinas;
+    private ArrayList<String> viajes;
+
+    public RadioS() {
+        super();
+        this.bocinas = false;
+    }
+
+    public void switch_bocinas() {
+        if (bocinas) {
+            bocinas = false;
+            System.out.println("Se cambio a auriculares");
+        } else {
+            bocinas = true;
+            System.out.println("Se cambio a bocinas");
+        }
+    }
+
+    public void planificar(Vista v) {
+        String viaje = v.datos_viaje();
+        System.out.println(viaje);
+    }
+
+    @Override
+    public String toString() {
+        String cadena = "";
+        return cadena;
+    }
