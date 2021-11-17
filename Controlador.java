8da8f9d4242017b872b43bb324f063e57ccbf9b9
Sebastián Silva <77755597+hsilv@users.noreply.github.com>
2021-11-17 03:31:56 +0000

    Se implementó el radio de clase C, además se creó su clase y sus funcionalidades específicas

--- Controlador.java
+++ Controlador.java
@@ -16,167 +16,373 @@ public class Controlador {
             while (index_accion1 != 6) {
                 index_accion1 = v.menu_p(radio);
                 if (index_accion1 == 1) {
+                    radio.setModo("Normal");
                     radio.turn();
                     System.out.println(radio);
-                    
+
                 } else if (index_accion1 == 2 && radio.isTurn()) {
+                    radio.setModo("Normal");
                     radio.vol(v.menu_vol());
                     System.out.println(radio);
-                    
+
                 } else if (index_accion1 == 3 && radio.isTurn()) {
                     index_accion2 = 0;
+                    radio.setModo("Radio");
                     while (index_accion2 != 5) {
                         index_accion2 = v.menu_r();
-                        
+
                         if (index_accion2 == 1) {
                             radio.Switch();
                             System.out.println(radio);
-                            
+
                         } else if (index_accion2 == 2) {
                             radio.cambiarEmisora(v.menu_emi());
                             System.out.println(radio);
-                            
+
                         } else if (index_accion2 == 3) {
                             radio.guardarEmisora(v.nombre_emisora());
                             System.out.println(radio);
-                            
+
                         } else if (index_accion2 == 4) {
                             radio.cargarEmisora(v);
                             System.out.println(radio);
                         }
-                        
+
                     }
-                } else if(index_accion1 == 4 && radio.isTurn()){
+                } else if (index_accion1 == 4 && radio.isTurn()) {
                     index_accion2 = 0;
-                    while(index_accion2 != 4){
+                    radio.setModo("Reproduccion");
+                    while (index_accion2 != 4) {
                         index_accion2 = v.menu_re();
-                        
-                        if(index_accion2 == 1){
+
+                        if (index_accion2 == 1) {
                             radio.seleccionarLista(v);
                             System.out.println(radio);
-                            
-                        } else if(index_accion2 == 2){
+
+                        } else if (index_accion2 == 2) {
                             radio.cambiarCancion(v);
                             System.out.println(radio);
-                            
-                        } else if(index_accion2 == 3){
+
+                        } else if (index_accion2 == 3) {
                             radio.escucharCancion(v);
                             System.out.println(radio);
                         }
                     }
-                    
-                } else if(index_accion1 == 5 && radio.isTurn()){
+
+                } else if (index_accion1 == 5 && radio.isTurn()) {
                     index_accion2 = 0;
-                    while(index_accion2 != 5){
+                    radio.setModo("Telefono");
+                    while (index_accion2 != 5) {
                         index_accion2 = v.menu_t(radio);
-                        
-                        if(index_accion2 == 1){
+
+                        if (index_accion2 == 1) {
                             radio.conec_desconec();
                             System.out.println(radio);
-                        } else if(index_accion2 == 2){
+                        } else if (index_accion2 == 2) {
                             radio.mostrarC(v);
                             System.out.println(radio);
-                        } else if(index_accion2 == 3){
+                        } else if (index_accion2 == 3) {
                             radio.llamarC(v);
                             System.out.println(radio);
-                        } else if(index_accion2 == 4){
+                        } else if (index_accion2 == 4) {
                             radio.finC();
                             System.out.println(radio);
                         }
                     }
-                } 
+                }
             }
         }
-        
+
         if (index_radio == 2) {
             RadioA radio = new RadioA();
             System.out.println(radio);
-            while (index_accion1 != 6) {
+            while (index_accion1 != 7) {
                 index_accion1 = v.menu_p(radio);
                 if (index_accion1 == 1) {
+                    radio.setModo("Normal");
                     radio.turn();
                     System.out.println(radio);
-                    
+
                 } else if (index_accion1 == 2 && radio.isTurn()) {
+                    radio.setModo("Normal");
                     radio.vol(v.menu_vol());
                     System.out.println(radio);
-                    
+
                 } else if (index_accion1 == 3 && radio.isTurn()) {
                     index_accion2 = 0;
+                    radio.setModo("Radio");
                     while (index_accion2 != 5) {
                         index_accion2 = v.menu_r();
-                        
+
                         if (index_accion2 == 1) {
                             radio.Switch();
                             System.out.println(radio);
-                            
+
                         } else if (index_accion2 == 2) {
                             radio.cambiarEmisora(v.menu_emi());
                             System.out.println(radio);
-                            
+
                         } else if (index_accion2 == 3) {
                             radio.guardarEmisora(v.nombre_emisora());
                             System.out.println(radio);
-                            
+
                         } else if (index_accion2 == 4) {
                             radio.cargarEmisora(v);
                             System.out.println(radio);
                         }
-                        
+
                     }
-                } else if(index_accion1 == 4 && radio.isTurn()){
+                } else if (index_accion1 == 4 && radio.isTurn()) {
                     index_accion2 = 0;
-                    while(index_accion2 != 4){
+                    radio.setModo("Reproduccion");
+                    while (index_accion2 != 4) {
                         index_accion2 = v.menu_re();
-                        
-                        if(index_accion2 == 1){
+
+                        if (index_accion2 == 1) {
                             radio.seleccionarLista(v);
                             System.out.println(radio);
-                            
-                        } else if(index_accion2 == 2){
+
+                        } else if (index_accion2 == 2) {
                             radio.cambiarCancion(v);
                             System.out.println(radio);
-                            
-                        } else if(index_accion2 == 3){
+
+                        } else if (index_accion2 == 3) {
                             radio.escucharCancion(v);
                             System.out.println(radio);
                         }
                     }
-                    
-                } else if(index_accion1 == 5 && radio.isTurn()){
+
+                } else if (index_accion1 == 5 && radio.isTurn()) {
                     index_accion2 = 0;
-                    while(index_accion2 != 6){
+                    radio.setModo("Telefono");
+                    while (index_accion2 != 6) {
                         index_accion2 = v.menu_t(radio);
-                        
-                        if(index_accion2 == 1){
+
+                        if (index_accion2 == 1) {
                             radio.conec_desconec();
                             System.out.println(radio);
-                        } else if(index_accion2 == 2){
+                        } else if (index_accion2 == 2) {
                             radio.mostrarC(v);
                             System.out.println(radio);
-                        } else if(index_accion2 == 3){
+                        } else if (index_accion2 == 3) {
                             radio.llamarC(v);
                             System.out.println(radio);
-                        } else if(index_accion2 == 4){
+                        } else if (index_accion2 == 4) {
                             radio.finC();
                             System.out.println(radio);
-                        } else if(index_accion2 == 5){
+                        } else if (index_accion2 == 5) {
                             radio.llam_ult();
                             System.out.println(radio);
                         }
                     }
-                } else if(index_accion1 == 6 && radio.isTurn()){
+                } else if (index_accion1 == 6 && radio.isTurn()) {
                     index_accion2 = 0;
-                    while(index_accion2 != 2){
-                        if(index_accion2 == 1){
+                    radio.setModo("Productividad");
+                    while (index_accion2 != 2) {
+                        index_accion2 = v.menu_pro(radio);
+
+                        if (index_accion2 == 1) {
                             radio.tarjetas(v);
                             System.out.println(radio);
                         }
                     }
-                } 
+                }
             }
         }
-        
+
+        if (index_radio == 3) {
+            RadioS radio = new RadioS();
+            System.out.println(radio);
+            while (index_accion1 != 7) {
+                index_accion1 = v.menu_p(radio);
+                if (index_accion1 == 1) {
+                    radio.setModo("Normal");
+                    radio.turn();
+                    System.out.println(radio);
+
+                } else if (index_accion1 == 2 && radio.isTurn()) {
+                    radio.setModo("Normal");
+                    radio.vol(v.menu_vol());
+                    System.out.println(radio);
+
+                } else if (index_accion1 == 3 && radio.isTurn()) {
+                    radio.setModo("Radio");
+                    index_accion2 = 0;
+                    while (index_accion2 != 5) {
+                        index_accion2 = v.menu_r();
+
+                        if (index_accion2 == 1) {
+                            radio.Switch();
+                            System.out.println(radio);
+
+                        } else if (index_accion2 == 2) {
+                            radio.cambiarEmisora(v.menu_emi());
+                            System.out.println(radio);
+
+                        } else if (index_accion2 == 3) {
+                            radio.guardarEmisora(v.nombre_emisora());
+                            System.out.println(radio);
+
+                        } else if (index_accion2 == 4) {
+                            radio.cargarEmisora(v);
+                            System.out.println(radio);
+                        }
+
+                    }
+                } else if (index_accion1 == 4 && radio.isTurn()) {
+                    index_accion2 = 0;
+                    radio.setModo("Reproduccion");
+                    while (index_accion2 != 4) {
+                        index_accion2 = v.menu_re();
+
+                        if (index_accion2 == 1) {
+                            radio.seleccionarLista(v);
+                            System.out.println(radio);
+
+                        } else if (index_accion2 == 2) {
+                            radio.cambiarCancion(v);
+                            System.out.println(radio);
+
+                        } else if (index_accion2 == 3) {
+                            radio.escucharCancion(v);
+                            System.out.println(radio);
+                        }
+                    }
+
+                } else if (index_accion1 == 5 && radio.isTurn()) {
+                    index_accion2 = 0;
+                    radio.setModo("Telefono");
+                    while (index_accion2 != 6) {
+                        index_accion2 = v.menu_t(radio);
+
+                        if (index_accion2 == 1) {
+                            radio.conec_desconec();
+                            System.out.println(radio);
+                        } else if (index_accion2 == 2) {
+                            radio.mostrarC(v);
+                            System.out.println(radio);
+                        } else if (index_accion2 == 3) {
+                            radio.llamarC(v);
+                            System.out.println(radio);
+                        } else if (index_accion2 == 4) {
+                            radio.finC();
+                            System.out.println(radio);
+                        } else if (index_accion2 == 5) {
+                            radio.switch_bocinas();
+                            System.out.println(radio);
+                        }
+                    }
+                } else if (index_accion1 == 6 && radio.isTurn()) {
+                    index_accion2 = 0;
+                    radio.setModo("Productividad");
+                    while (index_accion2 != 2) {
+                        index_accion2 = v.menu_pro(radio);
+
+                        if (index_accion2 == 1) {
+                            radio.planificar(v);
+                            System.out.println(radio);
+                        }
+                    }
+                }
+            }
+        }
+        if (index_radio == 4) {
+            RadioC radio = new RadioC();
+            System.out.println(radio);
+            while (index_accion1 != 7) {
+                index_accion1 = v.menu_p(radio);
+                if (index_accion1 == 1) {
+                    radio.setModo("Normal");
+                    radio.turn();
+                    System.out.println(radio);
+
+                } else if (index_accion1 == 2 && radio.isTurn()) {
+                    radio.setModo("Normal");
+                    radio.vol(v.menu_vol());
+                    System.out.println(radio);
+
+                } else if (index_accion1 == 3 && radio.isTurn()) {
+                    radio.setModo("Radio");
+                    index_accion2 = 0;
+                    while (index_accion2 != 5) {
+                        index_accion2 = v.menu_r();
+
+                        if (index_accion2 == 1) {
+                            radio.Switch();
+                            System.out.println(radio);
+
+                        } else if (index_accion2 == 2) {
+                            radio.cambiarEmisora(v.menu_emi());
+                            System.out.println(radio);
+
+                        } else if (index_accion2 == 3) {
+                            radio.guardarEmisora(v.nombre_emisora());
+                            System.out.println(radio);
+
+                        } else if (index_accion2 == 4) {
+                            radio.cargarEmisora(v);
+                            System.out.println(radio);
+                        }
+
+                    }
+                } else if (index_accion1 == 4 && radio.isTurn()) {
+                    index_accion2 = 0;
+                    radio.setModo("Reproduccion");
+                    while (index_accion2 != 4) {
+                        index_accion2 = v.menu_re();
+
+                        if (index_accion2 == 1) {
+                            radio.seleccionarLista(v);
+                            System.out.println(radio);
+
+                        } else if (index_accion2 == 2) {
+                            radio.cambiarCancion(v);
+                            System.out.println(radio);
+
+                        } else if (index_accion2 == 3) {
+                            radio.escucharCancion(v);
+                            System.out.println(radio);
+                        }
+                    }
+
+                } else if (index_accion1 == 5 && radio.isTurn()) {
+                    index_accion2 = 0;
+                    radio.setModo("Telefono");
+                    while (index_accion2 != 6) {
+                        index_accion2 = v.menu_t(radio);
+
+                        if (index_accion2 == 1) {
+                            radio.conec_desconec();
+                            System.out.println(radio);
+                        } else if (index_accion2 == 2) {
+                            radio.mostrarC(v);
+                            System.out.println(radio);
+                        } else if (index_accion2 == 3) {
+                            radio.llamarC(v);
+                            System.out.println(radio);
+                        } else if (index_accion2 == 4) {
+                            radio.finC();
+                            System.out.println(radio);
+                        } else if (index_accion2 == 5) {
+                            radio.espera();
+                            System.out.println(radio);
+                        }
+                    }
+                } else if (index_accion1 == 6 && radio.isTurn()) {
+                    index_accion2 = 0;
+                    radio.setModo("Productividad");
+                    while (index_accion2 != 2) {
+                        index_accion2 = v.menu_pro(radio);
+
+                        if (index_accion2 == 1) {
+                            radio.pronostico();
+                            System.out.println(radio);
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
 
